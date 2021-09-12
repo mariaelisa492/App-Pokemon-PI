e66f091205992b1ef3d33fc79f68fb353982b8d1
mariaelisa492 <mariaelisa004@hotmail.com>
2021-09-12 10:09:42 -0500

    PI API Pokemon

---
+++ api/src/models/Pokemon.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('pokemon', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life: {
+      type: DataTypes.INTEGER,
+    },
+    life: {
+      type: DataTypes.INTEGER,
+    },
+    power: {
+      type: DataTypes.INTEGER,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+    },
+  });
+};
